3ca48c28c6e7dfd9bb14ada6792b9efeb5bfe156
Nhat-An-Pham <phamnhatan01@gmail.com>
2022-05-27 16:14:27 +0700

    Update Chat Box UI

---
+++ src/assets/data/UserData.js
@@ -0,0 +1,8 @@
+const userData = {
+    userID: "01",
+    userName: "Riven",
+    userImg: require("../image/riven.png"),
+    fakeMsg: "My name is Riven"
+}
+
+export default userData;
--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,14 +1,32 @@
 import React from 'react'
 
+import userData from '../assets/data/UserData'
+
 const ChatBox = () => {
+
+
   return (
     <div className='chatBox'>
       <div className='chatBox__messageShow'>
+        <div className='chatBox__messageShow__item chatBox__messageShow__item--imgText'>
+          <div className='chatBox__messageShow__item--img'>
+            <img src={userData.userImg} alt="" />
+          </div>
+          <div className="chatBox__messageShow__item--content">
+            <div className='chatBox__messageShow__item__item--nameTime'>
+              <p2>{userData.userName} - </p2>
+              <p2>Now</p2>
+            </div>
+            <div className='chatBox__messageShow__item__item--msg'>
+              <p>{userData.fakeMsg}</p>
+            </div>
+          </div>
+        </div>
 
       </div>
       <div className='chatBox__inputMessage'>
-          <input className='chatBox__inputMessage__item chatBox__inputMessage__item--input' placeholder='Input Message'/>
-          <button className='chatBox__inputMessage__item chatBox__inputMessage__item--btn'>SEND</button>
+        <input className='chatBox__inputMessage__item chatBox__inputMessage__item--input' placeholder='Input Message' />
+        <button className='chatBox__inputMessage__item chatBox__inputMessage__item--btn'>SEND</button>
       </div>
     </div>
   )
